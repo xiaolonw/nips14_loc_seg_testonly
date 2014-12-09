7f814b3048a2912943bd7ac594856aff98a1c103
dragon123 <dragon123@dragon123-M51AC.(none)>
2014-12-09 16:09:09 -0500

    rm files

--- Caffe_Segmentation/segscripts/seg/compute_seg_mean.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-rootfolder=/nfs/hn46/xiaolonw/cnncode/caffe-3dnormal_r_n
-
-GLOG_logtostderr=1 $rootfolder/build/tools/demo_compute_image_mean.bin /nfs/ladoga_no_backups/users/xiaolonw/oedata/leveldb/seg_train_db   /nfs/ladoga_no_backups/users/xiaolonw/oedata/leveldb/seg_train_db/3d_mean.binaryproto
