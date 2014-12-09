94318191483d53f2936898b6815b1306569371f8
dragon123 <dragon123@dragon123-M51AC.(none)>
2014-12-09 15:59:04 -0500

    rm files

--- src/Caffe_Segmentation/segscripts/loc/crop_by_loc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rootfolder=/exports/cyclops/work/001_Selfies/002_Segmentation/src/Caffe_Segmentation
-
-GLOG_logtostderr=1 $rootfolder/build/examples/seg/cropDet.bin \
-    /exports/cyclops/work/001_Selfies/002_Segmentation/src/Caffe_Segmentation/results/backpage/ImagesNevada/loc/locResult.txt \
-    /exports/cyclops/work/003_Backpage/dataset/backpage/corpus \
-    /exports/cyclops/work/001_Selfies/002_Segmentation/src/Caffe_Segmentation/results/backpage/ImagesNevada/crops
-
