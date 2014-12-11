90edbf9e299e9466f1a176c69eb1bd7ebf8d996b
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2014-12-11 01:23:46 +0000

    added paths to model/data files

--- Caffe_Segmentation/segscripts/get_seg_loc_models.sh
+++ Caffe_Segmentation/segscripts/get_seg_loc_models.sh
@@ -1,8 +1,8 @@
 mkdir -p models
 cd models
 
-wget /path/to/seg.caffemodel models/seg.caffemodel
-wget /path/to/loc.caffemodel models/loc.caffemodel
-wget /path/to/seg_mean.binaryproto models/seg_mean.binaryproto
-wget /path/to/loc_mean.binaryproto models/loc_mean.binaryproto
+wget https://www.dropbox.com/sh/t851g3hxzgisgit/AABNm6PjYi5rvrr8ADOUufX7a/seg.caffemodel
+wget https://www.dropbox.com/sh/t851g3hxzgisgit/AAAG5OXDbnFmD1cXNWsrxQyFa/loc.caffemodel
+wget https://www.dropbox.com/sh/t851g3hxzgisgit/AAC7dCiBM31IdM2ir83rZo67a/seg_mean.binaryproto
+wget https://www.dropbox.com/sh/t851g3hxzgisgit/AAD3YBBXEcUHRrGvGCwq03IFa/imagenet_mean.binaryproto loc_mean.binaryproto
 
