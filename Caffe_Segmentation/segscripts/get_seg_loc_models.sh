083a02757150ec977189facbb4351999c64e2de1
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2014-12-11 01:10:43 +0000

    minor bug in data script

--- Caffe_Segmentation/segscripts/get_seg_loc_models.sh
+++ Caffe_Segmentation/segscripts/get_seg_loc_models.sh
@@ -1,8 +1,8 @@
 mkdir -p models
 cd models
 
-wget /path/to/seg.caffemodel seg.caffemodel
-wget /path/to/loc.caffemodel loc.caffemodel
-wget /path/to/seg_mean.binaryproto seg_mean.binaryproto
-wget /path/to/loc_mean.binaryproto loc_mean.binaryproto
+wget /path/to/seg.caffemodel models/seg.caffemodel
+wget /path/to/loc.caffemodel models/loc.caffemodel
+wget /path/to/seg_mean.binaryproto models/seg_mean.binaryproto
+wget /path/to/loc_mean.binaryproto models/loc_mean.binaryproto
 
